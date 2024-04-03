765742bd76f0237674a02106c9b6f70bc5f6e48d
Vladis Markin <vladismarkin@gmail.com>
2024-04-03 21:21:36 +0300

    start work on table

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Container } from "@mui/material";
-import TimeSeriesChart from "./components/TimeSeriesChart";
+// import TimeSeriesChart from "./components/TimeSeriesChart";
 import AppTitle from "./components/AppTitle";
+import MetricsTable from "./components/MetricsTable";
 
 export default function App() {
   return (
     <Container>
       <AppTitle />
-      <TimeSeriesChart />
+      {/* <TimeSeriesChart /> */}
+      <MetricsTable />
     </Container>
   );
 }
